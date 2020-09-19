9f51a365d4095ca649331e7cbb360e696bc4a0b6
Anas Elkinawy <60526687+anasemqenawy@users.noreply.github.com>
2020-09-19 10:17:35 +0200

    Update FK_IK_Blend.py

--- FK_IK_Blend.py
+++ FK_IK_Blend.py
@@ -5,6 +5,7 @@ def fk_ik_blend():
     fk_list = []
     ik_list = []
     sel = cmds.ls(sl=True)
+    #---------------------------------------------------------
     for i in sel:
         x=i
         if "_fk" in x:
@@ -13,9 +14,13 @@ def fk_ik_blend():
             ik_list.append(x)
         else:
             jnt_list.append(x)
+    #---------------------------------------------------------
     cmds.parentConstraint(fk_list[0], ik_list[0], jnt_list[0], mo=0)
     cmds.parentConstraint(fk_list[1], ik_list[1], jnt_list[1], mo=0)
     cmds.parentConstraint(fk_list[2], ik_list[2], jnt_list[2], mo=0)
-
-if __init__ == "main":
+#---------------------------------------------------------
+if __name__ == "__main__":
     fk_ik_blend()
+
+
+
