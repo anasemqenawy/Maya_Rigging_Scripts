4bb310c8269a02bbece4b97d11875b09be497db9
Anas Elkinawy <60526687+anasemqenawy@users.noreply.github.com>
2020-09-19 15:27:16 +0200

    Update FK_IK_Blend.py

--- FK_IK_Blend.py
+++ FK_IK_Blend.py
@@ -1,6 +1,18 @@
+import sys
 import maya.cmds as cmds
 #---------------------------------------------------------
-def fk_ik_blend():
+def fk_ik_blend(*args):
+    #---------------------------------------------------------
+    swtch_ctrl = cmds.textField("name", query=True, text=True)
+    if swtch_ctrl == None or cmds.objExists(swtch_ctrl)==False:
+            swtch_ctrl = cmds.circle(name = "Fk_Ik_Switch", normal=(0,1,0), radius=3)[0]
+    else:
+            swtch_ctrl = cmds.duplicate(swtch_ctrl, name="Fk_Ik_Switch")[0]
+    cmds.addAttr(swtch_ctrl, ln="FK_IK", at= "double", min = 0, max= 1, keyable=1,hidden=0, readable=1)
+    grp = cmds.group(em=True, name="{}_Grp".format(swtch_ctrl))
+    cmds.parent(swtch_ctrl, grp)
+    cmds.DeleteHistory(swtch_ctrl)
+    #---------------------------------------------------------
     jnt_list = []
     fk_list = []
     ik_list = []
@@ -15,6 +27,10 @@ def fk_ik_blend():
         else:
             jnt_list.append(x)
     #---------------------------------------------------------
+    l = len(jnt_list)
+    pc = cmds.pointConstraint(jnt_list[l-1], grp, mo=0)
+    cmds.delete(pc)
+    #---------------------------------------------------------
     shouldr_constraint = jnt_list[0]+"_constraint"
     shouldr_list = shouldr_constraint.split("_")
     elbow_constraint = jnt_list[1]+"_constraint"
@@ -24,17 +40,7 @@ def fk_ik_blend():
     cmds.parentConstraint(fk_list[0], ik_list[0], jnt_list[0], mo=0, name="{}".format(shouldr_constraint))
     cmds.parentConstraint(fk_list[1], ik_list[1], jnt_list[1], mo=0, name="{}".format(elbow_constraint))
     cmds.parentConstraint(fk_list[2], ik_list[2], jnt_list[2], mo=0, name="{}".format(wrst_constraint))
-    #---------------------------------------------------------
-    swtch_ctrl = "Fk_Ik_Switch"
-    cmds.circle(name="{}".format(swtch_ctrl), normal=(0,1,0), r=3)
-    cmds.addAttr(ln="FK_IK", at= "double", min = 0, max= 1, keyable=1,hidden=0, readable=1)
-    grp = cmds.group(em=True, name="{}_Grp".format("Fk_Ik_Switch"))
-    cmds.parent(swtch_ctrl, grp)
-    cmds.DeleteHistory(swtch_ctrl)
-    #---------------------------------------------------------
-    l = len(jnt_list)
-    pc = cmds.pointConstraint(jnt_list[l-1], grp, mo=0)
-    cmds.delete(pc)
+    
     #---------------------------------------------------------
     rev_node = "fk_ik_reverse"
     if rev_node == None or cmds.objExists(rev_node)==False:
@@ -50,3 +56,4 @@ def fk_ik_blend():
     cmds.connectAttr("fk_ik_reverse.outputX", "{}.{}_{}_fkW0".format(elbow_constraint, elbow_list[0], elbow_list[1]))
     cmds.connectAttr("fk_ik_reverse.outputX", "{}.{}_{}_fkW0".format(wrst_constraint, wrst_list[0], wrst_list[1]))
     #---------------------------------------------------------
+fk_ik_blend() 
