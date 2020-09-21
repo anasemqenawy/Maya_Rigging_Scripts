8bb2fde99a4db70b6329e105a2ea48fd7beb195f
Anas Elkinawy <60526687+anasemqenawy@users.noreply.github.com>
2020-09-21 02:44:16 +0200

    Update FK_IK_Blend.py

--- FK_IK_Blend.py
+++ FK_IK_Blend.py
@@ -2,58 +2,54 @@ import sys
 import maya.cmds as cmds
 #---------------------------------------------------------
 def fk_ik_blend(*args):
-    #---------------------------------------------------------
+     #---------------------------------------------------------
     swtch_ctrl = cmds.textField("name", query=True, text=True)
     if swtch_ctrl == None or cmds.objExists(swtch_ctrl)==False:
             swtch_ctrl = cmds.circle(name = "Fk_Ik_Switch", normal=(0,1,0), radius=3)[0]
     else:
             swtch_ctrl = cmds.duplicate(swtch_ctrl, name="Fk_Ik_Switch")[0]
     cmds.addAttr(swtch_ctrl, ln="FK_IK", at= "double", min = 0, max= 1, keyable=1,hidden=0, readable=1)
-    grp = cmds.group(em=True, name="{}_Grp".format(swtch_ctrl))
-    cmds.parent(swtch_ctrl, grp)
+    swtch_ctrl_grp = cmds.group(em=True, name="{}_Grp".format(swtch_ctrl))
+    cmds.parent(swtch_ctrl, swtch_ctrl_grp)
     cmds.DeleteHistory(swtch_ctrl)
     #---------------------------------------------------------
+    sel = cmds.ls(sl=True)
     jnt_list = []
     fk_list = []
     ik_list = []
-    sel = cmds.ls(sl=True)
-    #---------------------------------------------------------
     for i in sel:
-        x=i.lower()
+        x = i.lower()
         if "_fk" in x:
             fk_list.append(x)
-        if "_ik" in x:
+        elif "_ik" in x:
             ik_list.append(x)
         else:
             jnt_list.append(x)
     #---------------------------------------------------------
-    l = len(jnt_list)
-    pc = cmds.pointConstraint(jnt_list[l-1], grp, mo=0)
+    pc = cmds.pointConstraint(jnt_list[2], swtch_ctrl_grp, mo=0)
     cmds.delete(pc)
     #---------------------------------------------------------
-    shouldr_constraint = jnt_list[0]+"_constraint"
-    shouldr_list = shouldr_constraint.split("_")
-    elbow_constraint = jnt_list[1]+"_constraint"
-    elbow_list = elbow_constraint.split("_")
-    wrst_constraint = jnt_list[2]+"_constraint"
-    wrst_list = wrst_constraint.split("_")
-    cmds.parentConstraint(fk_list[0], ik_list[0], jnt_list[0], mo=0, name="{}".format(shouldr_constraint))
-    cmds.parentConstraint(fk_list[1], ik_list[1], jnt_list[1], mo=0, name="{}".format(elbow_constraint))
-    cmds.parentConstraint(fk_list[2], ik_list[2], jnt_list[2], mo=0, name="{}".format(wrst_constraint))
-    
+    shoulder_blend_node = "FK_IK_{}_Blend".format(jnt_list[0])
+    elbow_blend_node = "FK_IK_{}_Blend".format(jnt_list[1])
+    wrist_blend_node = "FK_IK_{}_Blend".format(jnt_list[2])
     #---------------------------------------------------------
-    rev_node = "fk_ik_reverse"
-    if rev_node == None or cmds.objExists(rev_node)==False:
-        cmds.shadingNode("reverse", name = "{}".format(rev_node), asUtility=True)
-    else:
-        rev_node = rev_node
-    cmds.connectAttr("Fk_Ik_Switch.FK_IK", "{}.{}_{}_ikW1".format(shouldr_constraint, shouldr_list[0], shouldr_list[1]))
-    cmds.connectAttr("Fk_Ik_Switch.FK_IK", "{}.{}_{}_ikW1".format(elbow_constraint, elbow_list[0], elbow_list[1]))
-    cmds.connectAttr("Fk_Ik_Switch.FK_IK", "{}.{}_{}_ikW1".format(wrst_constraint, wrst_list[0], wrst_list[1]))
-    cmds.connectAttr("Fk_Ik_Switch.FK_IK", "fk_ik_reverse.inputX")
+    cmds.shadingNode("blendColors", name=shoulder_blend_node, asUtility=True)
+    cmds.connectAttr("{}.rotate".format(fk_list[0]), "{}.color2".format(shoulder_blend_node))
+    cmds.connectAttr("{}.rotate".format(ik_list[0]), "{}.color1".format(shoulder_blend_node))
+    cmds.connectAttr("{}.FK_IK".format(swtch_ctrl), "{}.blender".format(shoulder_blend_node))
+    cmds.connectAttr("{}.output".format(shoulder_blend_node), "{}.rotate".format(jnt_list[0]))
     #---------------------------------------------------------
-    cmds.connectAttr("fk_ik_reverse.outputX", "{}.{}_{}_fkW0".format(shouldr_constraint, shouldr_list[0], shouldr_list[1]))
-    cmds.connectAttr("fk_ik_reverse.outputX", "{}.{}_{}_fkW0".format(elbow_constraint, elbow_list[0], elbow_list[1]))
-    cmds.connectAttr("fk_ik_reverse.outputX", "{}.{}_{}_fkW0".format(wrst_constraint, wrst_list[0], wrst_list[1]))
+    cmds.shadingNode("blendColors", name=elbow_blend_node.format(jnt_list[1]), asUtility=True)
+    cmds.connectAttr("{}.rotate".format(fk_list[1]), "{}.color2".format(elbow_blend_node))
+    cmds.connectAttr("{}.rotate".format(ik_list[1]), "{}.color1".format(elbow_blend_node))
+    cmds.connectAttr("{}.FK_IK".format(swtch_ctrl), "{}.blender".format(elbow_blend_node))
+    cmds.connectAttr("{}.output".format(elbow_blend_node), "{}.rotate".format(jnt_list[1]))
     #---------------------------------------------------------
-fk_ik_blend() 
+    cmds.shadingNode("blendColors", name=wrist_blend_node.format(jnt_list[2]), asUtility=True)
+    cmds.connectAttr("{}.rotate".format(fk_list[2]), "{}.color2".format(wrist_blend_node))
+    cmds.connectAttr("{}.rotate".format(ik_list[2]), "{}.color1".format(wrist_blend_node))
+    cmds.connectAttr("{}.FK_IK".format(swtch_ctrl), "{}.blender".format(wrist_blend_node))
+    cmds.connectAttr("{}.output".format(wrist_blend_node), "{}.rotate".format(jnt_list[2]))
+    #---------------------------------------------------------
+
+fk_ik_blend(*args)
